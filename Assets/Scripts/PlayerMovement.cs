fd333933b71dd9fd67ce4c40962b54def23208dc
arcanebrownie <75907336+arcanebrownie@users.noreply.github.com>
2023-03-04 16:04:00 -0700

    Moved dirs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -17,9 +19,20 @@ public class PlayerMovement : MonoBehaviour
     {
         float dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2 (dirX * 7f, rb.velocity.y);
+
         if (Input.GetKeyDown("space"))
         {
             rb.velocity = new Vector2(rb.velocity.x, 3f);
+        }
+
+        if (dirX > 0)
+        {
+            animator.SetBool("isWalking", true);
+        } else if (dirX < 0) {
+            animator.SetBool("isWalking", true);
+        } else
+        {
+            animator.SetBool("isWalking", false);
         }
     }
 }
